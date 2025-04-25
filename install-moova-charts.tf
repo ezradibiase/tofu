71402f4a359075cbd818d336fb9f57ac16f17bcf
ezrad@fangorn <ezradibiase@gmail.com>
2025-04-25 16:10:14 +0200

    Primi test

--- install-moova-charts.tf
+++ install-moova-charts.tf
@@ -2,6 +2,7 @@ terraform {
   required_providers {
     helm = {
       source = "opentofu/helm"
+      version = "3.0.0-pre2"
     }
     kubernetes = {
     }
@@ -9,13 +10,16 @@ terraform {
 }
 
 provider "helm" {
+  debug = true
   kubernetes = {
     config_path    = "~/.kube/config"
     config_context = "docker-desktop"
   }
   registries = [
     {
-      url     = ""
+      url       = "oci://moovaregistry.almaviva.it:5050/gitlab-instance-a5bf24f7/moova"
+      username  = "root"
+      password  = "dtE5sPAA9scGYCn56qzh"      
     }
   ]
 }
@@ -25,7 +29,7 @@ provider "kubernetes" {
   config_context = "docker-desktop"
 }
 
-resource "kubernetes_namespace" "example" {
+resource "kubernetes_namespace" "ns-moova" {
   metadata {
     name = "ns-moova-terraform"
   }
@@ -35,5 +39,37 @@ resource "helm_release" "cert-manager" {
   name       = "cert-manager"
   repository = "https://charts.jetstack.io"
   chart      = "cert-manager"
-  namespace  = "my-first-namespace"
+  namespace  = "cert-manager"
+  create_namespace  = true
+
+  set = [
+    {
+      name  = "installCRDs"
+      value = "true"
+    }
+    /*
+      Nel caso in cui l’installazione avvenga su EKS con CNI custom
+    {
+      name  = "webhook.hostNetwork"
+      value = "true"
+    },
+    {
+      name  = "webhook.securePort"
+      value = "10251"
+    }
+  */
+  ]
+}
+
+resource "helm_release" "prerequisiti" {
+  name        = "prerequisiti"
+  chart       = "oci://moovaregistry.almaviva.it:5050/gitlab-instance-a5bf24f7/moova/prerequisiti"
+  namespace   = "kube-system"
+
+  set = [
+    {
+      name  = "certManager.selfSigned"
+      value = "true"
+    }
+  ]
 }
